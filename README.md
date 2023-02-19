08690249e7102ca016d9203d4db6e595315f9dfc
Pratik Sahoo <22b0968@iitb.ac.in>
2023-02-19 10:10:04 +0530

    First complete version of arena.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Genetic Optimisation
 ### Contributers: Pratik Sahoo, Yash Jonjale
-This is a repository from which our project to genetically optimise models that play 2-player games will be based. Will update this readme as project progresses. 
+This is a repository from which our project to genetically optimise models that play 2-player games will be based. 
+
+arena.py is now completed, providing an Arena class for genetic optimization. The requirements of the game and model are detailed in a comment at the start of the class. Further updates to the same will be detailed here. 
--- arena.py
+++ arena.py
@@ -1,33 +1,35 @@
-import numpy as np
 import random as rand
+import tqdm.tqdm as TQDM
 
 class Arena():
     """
     What we expect of model_class: 
     An __init__ that takes a list of parameters 
-    A function that can crossover between 2 different instances of model_class and create a 3rd and 4th
+    A function crossover(self, other_model) that can crossover between 2 different instances of model_class and return a 3rd and 4th 
+    A function mutate(self) that can mutate a model and return mutant. Preferably does high probability of small mutations and low probability of huge mutations
+        Note: The methods of crossover and mutation, whether discrete or intermediate, and with what values of crossover_d, are considered attributes of the inhabitants, not the environment. hence, they are left out of this class.
 
     What we expect of game:
     An __init__ with the option of slightly random beginning positions
     A method play(self, agent1, agent2, standard_start = True) which plays out a game between agent 1, agent 2 and return 1, 2 or 3 if player 1 win, player 2 win or tie respectively
     """
 
-    def __init__(self, model_class, game, model_params = [], initial_pop = 300, max_pop = 500, crossover_rate = 0.5, mutation_rate = 0.05, battle_stochastic = 0.1):
+    def __init__(self, model_class, game, model_params = [], initial_pop = 300, max_pop = 500, crossover_rate = 0.1, mutation_rate = 0.07, battle_stochastic = 0.1):
         self.inhabitants = []
         self.game = game
         self.model_class = model_class
         self.initial_pop = initial_pop
-        self.model_params = model_params
+        self.model_params = model_params # parameters (NOT GENES) for initialization of inhabitants. There must still be some randomness in their initialization
         self.crossover_rate = crossover_rate
         self.mutation_rate = mutation_rate
         self.battle_stochastic = battle_stochastic # Chance that the loser in fight_pair survives IMPORTANT: should be less than 0.5
 
-    
-
-    def initialize(self):
         for _ in range(self.initial_pop):
             self.inhabitants.append(self.model_class(self.model_params))
+
     
+
+
     def pop_cutdown(self):
         while True:
             if len(self.inhabitants)<=self.max_pop:
@@ -51,10 +53,30 @@ class Arena():
                 
             else:
                 self.inhabitants.pop(fight_pair[0])
+    
+    def reproduction(self): # Takes care of crossovers AND mutations
+        for i in range(self.inhabitants):
+            for j in range(self.inhabitants):
+                if i==j:
+                    continue
 
-
-            
-            
-            
+                tt = rand.random()
+                if tt<self.crossover_rate:
+                    child1, child2 = self.inhabitants[i].crossover(self.inhabitants[j])
+                    self.inhabitants.append(child1)
+                    self.inhabitants.append(child2)
         
-
+        for i in range(self.inhabitants):
+            tt = rand.random()
+            if tt<self.mutation_rate:
+                a = self.inhabitants[i].mutate()
+                self.inhabitants.append(a)
+    
+    def train(self, num_gens = 1000):
+        
+        for i in TQDM(range(num_gens)):
+            if i%100 == 99:
+                print("Generation %d \n", i+1)
+            
+            self.reproduction()
+            self.pop_cutdown()
